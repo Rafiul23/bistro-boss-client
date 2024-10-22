e8e8fe93f78b2215d8469774d9ab14adb905fd89
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-10-22 19:28:25 +0600

    loaded menu items

--- src/pages/Home/PopularMenu/PopularMenu.jsx
+++ src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,14 +1,27 @@
+import { useEffect, useState } from "react";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 
 const PopularMenu = () => {
-    return (
-        <section>
-            <SectionTitle
-            heading={'From Our Menu'}
-            subHeading={'Check it out'}
-            ></SectionTitle>
-        </section>
-    );
+  const [menu, setMenu] = useState([]);
+
+  useEffect(() => {
+    fetch("menu.json")
+      .then((res) => res.json())
+      .then((data) => {
+        const popularItems = data.filter(item => item.category === 'popular')
+        setMenu(popularItems)
+    });
+  }, []);
+
+  return (
+    <section>
+      <SectionTitle
+        heading={"From Our Menu"}
+        subHeading={"Check it out"}
+      ></SectionTitle>
+      <h2>{menu.length}</h2>
+    </section>
+  );
 };
 
 export default PopularMenu;
