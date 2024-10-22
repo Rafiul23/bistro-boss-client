125f475cf0d3d725729d3229d43f5187ef4848ad
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-10-22 20:13:05 +0600

    started working at featured section

---
+++ src/pages/Home/Featured/Featured.jsx
@@ -0,0 +1,14 @@
+import SectionTitle from '../../../components/SectionTitle/SectionTitle';
+import featuredImg from '../../../assets/home/featured.jpg';
+const Featured = () => {
+    return (
+        <div>
+           <SectionTitle
+           subHeading={'Check it out'}
+           heading={'From our menu'}
+           ></SectionTitle> 
+        </div>
+    );
+};
+
+export default Featured;
