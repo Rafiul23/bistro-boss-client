8d7f4e288729fd4768f94359fa19fc6e74ca450f
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-10-22 19:17:07 +0600

    started working with popular menu

---
+++ src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -0,0 +1,14 @@
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+
+const PopularMenu = () => {
+    return (
+        <section>
+            <SectionTitle
+            heading={'From Our Menu'}
+            subHeading={'Check it out'}
+            ></SectionTitle>
+        </section>
+    );
+};
+
+export default PopularMenu;
