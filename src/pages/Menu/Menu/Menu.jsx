935e3560bec14a3c4bab16ef1649b334ae88f884
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-10-23 21:12:35 +0600

    added all category food in menu item

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,10 +1,10 @@
 import { Helmet } from "react-helmet-async";
 import Cover from "../../Shared/Cover/Cover";
 import menu_bg from "../../../assets/menu/banner3.jpg";
-import desert_img from '../../../assets/menu/dessert-bg.jpeg';
-import pizza_img from '../../../assets/menu/pizza-bg.jpg';
-import salad_img from '../../../assets/menu/salad-bg.jpg';
-import soup_img from '../../../assets/menu/soup-bg.jpg';
+import desert_img from "../../../assets/menu/dessert-bg.jpeg";
+import pizza_img from "../../../assets/menu/pizza-bg.jpg";
+import salad_img from "../../../assets/menu/salad-bg.jpg";
+import soup_img from "../../../assets/menu/soup-bg.jpg";
 import useMenu from "../../../hooks/useMenu";
 import SectionTitle from "./../../../components/SectionTitle/SectionTitle";
 import MenuCategory from "../MenuCategory/MenuCategory";
@@ -39,58 +39,44 @@ const Menu = () => {
           Order Your Favourite Food
         </button>
       </div>
-    
-    <MenuItem
-    img={desert_img}
-    title={'Deserts'}
-    subTitle={'Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'}
-    items={dessert}
-    ></MenuItem>
 
-      
+      <MenuItem
+        img={desert_img}
+        title={"Deserts"}
+        subTitle={
+          "Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        }
+        items={dessert}
+      ></MenuItem>
 
-      <div className="mb-10">
-      <Cover
+      <MenuItem
         img={pizza_img}
         title={"Pizza"}
-        subTitle={"Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."}
-      ></Cover>
-      </div>
-      <MenuCategory items={pizza}></MenuCategory>
-      <div className="text-center my-10">
-        <button className="btn btn-outline border-0 border-b-4">
-          Order Your Favourite Food
-        </button>
-      </div>
+        subTitle={
+          "Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        }
+        items={pizza}
+      ></MenuItem>
 
-      <div className="mb-10">
-      <Cover
+      <MenuItem
         img={salad_img}
         title={"Salads"}
-        subTitle={"Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."}
-      ></Cover>
-      </div>
-      <MenuCategory items={salad}></MenuCategory>
-      <div className="text-center my-10">
-        <button className="btn btn-outline border-0 border-b-4">
-          Order Your Favourite Food
-        </button>
-      </div>
+        subTitle={
+          "Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        }
+        items={salad}
+      ></MenuItem>
 
-     <div className="mb-10">
-     <Cover
+
+      <MenuItem
         img={soup_img}
         title={"Soups"}
-        subTitle={"Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."}
-      ></Cover>
-     </div>
-      <MenuCategory items={soup}></MenuCategory>
-      <div className="text-center my-10">
-        <button className="btn btn-outline border-0 border-b-4">
-          Order Your Favourite Food
-        </button>
-      </div>
-
+        subTitle={
+          "Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        }
+        items={soup}
+      ></MenuItem>
+    
     </div>
   );
 };
