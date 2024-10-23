f65fbdbbe1fb3b9e64dd7e8b880c4a34ef235fea
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-10-23 22:40:32 +0600

    implementing tab system in order page

--- src/pages/Order/Order.jsx
+++ src/pages/Order/Order.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import orderImg from "../../assets/order/order.jpg";
+import Cover from "../Shared/Cover/Cover";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 
 const Order = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div>
+      <Cover
+        img={orderImg}
+        title={"Our Shop"}
+        subTitle={"Would you like to try a dish?"}
+      ></Cover>
+      <Tabs>
+        <TabList>
+          <Tab>Title 1</Tab>
+          <Tab>Title 2</Tab>
+        </TabList>
+
+        <TabPanel>
+          <h2>Any content 1</h2>
+        </TabPanel>
+        <TabPanel>
+          <h2>Any content 2</h2>
+        </TabPanel>
+      </Tabs>
+    </div>
+  );
 };
 
 export default Order;
