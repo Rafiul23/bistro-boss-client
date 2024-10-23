aba3d7fc3ba2b5608d1e4d76c392c01f092c8496
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-10-23 22:51:53 +0600

    menu data is loaded and filtered

--- src/pages/Order/Order.jsx
+++ src/pages/Order/Order.jsx
@@ -1,9 +1,20 @@
+import { useState } from "react";
 import orderImg from "../../assets/order/order.jpg";
 import Cover from "../Shared/Cover/Cover";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
+import useMenu from "../../hooks/useMenu";
 
 const Order = () => {
+  const [tabIndex, setTabIndex] = useState(1);
+  const [menu] = useMenu();
+
+  const dessert = menu.filter((item) => item.category === "dessert");
+  const pizza = menu.filter((item) => item.category === "pizza");
+  const salad = menu.filter((item) => item.category === "salad");
+  const soup = menu.filter((item) => item.category === "soup");
+  const drinks = menu.filter((item) => item.category === "drinks");
+  
   return (
     <div>
       <Cover
@@ -11,10 +22,13 @@ const Order = () => {
         title={"Our Shop"}
         subTitle={"Would you like to try a dish?"}
       ></Cover>
-      <Tabs>
+      <Tabs defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
         <TabList>
-          <Tab>Title 1</Tab>
-          <Tab>Title 2</Tab>
+          <Tab>Salad</Tab>
+          <Tab>Pizza</Tab>
+          <Tab>Soups</Tab>
+          <Tab>Desserts</Tab>
+          <Tab>Drinks</Tab>
         </TabList>
 
         <TabPanel>
