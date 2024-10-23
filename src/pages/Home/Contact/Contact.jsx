99d27ded3732ef7db238a2b663930862171ea4da
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-10-23 15:20:37 +0600

    added contact section

---
+++ src/pages/Home/Contact/Contact.jsx
@@ -0,0 +1,10 @@
+
+const Contact = () => {
+    return (
+        <section className="max-w-screen-xl h-[250px] bg-[#151515] flex justify-center items-center my-20">
+            <h2 className="text-white text-4xl">Call Us: +88 0192345678910</h2>
+        </section>
+    );
+};
+
+export default Contact;
