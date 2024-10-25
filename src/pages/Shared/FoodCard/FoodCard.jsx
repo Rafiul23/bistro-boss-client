bdb4ef8ed6d747922f9064fb5438b254200f8470
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-10-25 20:27:11 +0600

    reviews are fetched from db successfully

--- src/pages/Shared/FoodCard/FoodCard.jsx
+++ src/pages/Shared/FoodCard/FoodCard.jsx
@@ -6,11 +6,11 @@ const FoodCard = ({item}) => {
         <img src={image} className="w-full" alt="Food" />
         <p className="bg-slate-900 absolute top-2 right-2 text-white px-2 py-1 rounded-lg"> ${price}</p>
       </figure>
-      <div className="card-body text-center">
+      <div className="card-body text-center space-y-2">
         <h2 className="text-3xl font-bold">{name}</h2>
         <p>{recipe}</p>
         <div className="card-actions justify-center">
-          <button className="btn btn-outline border-0 hover:text-amber-400 border-b-4 text-amber-400 border-b-amber-400 uppercase">
+          <button className="btn btn-outline bg-slate-200 border-0 hover:text-amber-400 border-b-4 text-amber-400 border-b-amber-400 uppercase">
             Add to cart
           </button>
         </div>
