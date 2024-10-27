1deb39fae7a30b4cad2eb71a9064df2e5f4c42b7
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-10-27 23:54:34 +0600

    setup onAuthStateChanged

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,13 +1,33 @@
-const { createContext } = require("react");
+const { createContext, useState, useEffect } = require("react");
+import { getAuth, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
+import { app } from './../firebase/firebase.config';
 
 export const AuthContext = createContext(null);
+
+const auth = getAuth(app);
 
 const AuthProvider = ({children})=>{
 
 
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    
+
+    useEffect(()=>{
+      const unSubscribe =  onAuthStateChanged(auth, (currentUser)=>{
+            setUser(currentUser);
+            setLoading(false);
+        });
+      
+        return ()=>{
+           return unSubscribe();
+        }
+    },[])
 
     const authInfo = {
-
+        user,
+        loading
     };
 
     return (<AuthContext.Provider value={authInfo}>
@@ -15,4 +35,5 @@ const AuthProvider = ({children})=>{
     </AuthContext.Provider>
     )
 }
+
 export default AuthProvider;
