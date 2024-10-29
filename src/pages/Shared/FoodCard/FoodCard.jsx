c6fa3704bc15b8dd37b76cfcb2049efa70d59e89
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-10-29 19:04:32 +0600

    add to cart is successful

--- src/pages/Shared/FoodCard/FoodCard.jsx
+++ src/pages/Shared/FoodCard/FoodCard.jsx
@@ -1,5 +1,53 @@
+import Swal from "sweetalert2";
+import useAuth from "../../../hooks/useAuth";
+import { useLocation, useNavigate } from "react-router-dom";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+
 const FoodCard = ({item}) => {
-    const {image, name, recipe, price} = item;
+    const {image, name, recipe, price, _id} = item;
+    const {user} = useAuth();
+    const navigate = useNavigate();
+    const location = useLocation();
+    const axiosSecure = useAxiosSecure();
+
+
+    const handleAddToCart = food =>{
+      if(user && user?.email){
+        const cartItem = {
+          menuId: _id,
+          email: user.email,
+          name,
+          image,
+          price
+        }
+
+        axiosSecure.post('/carts', cartItem)
+        .then(res =>{
+          if(res.data.insertedId){
+            Swal.fire({
+              title: "Added to Cart!",
+              text: "Your item has been added to cart.",
+              icon: "success"
+            });
+          }
+        })
+
+      } else {
+        Swal.fire({
+          title: "You are not logged in",
+          text: "Please login to add to the cart",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#3085d6",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "Go to Login!"
+        }).then((result) => {
+          if (result.isConfirmed) {
+            navigate('/login', {state: {from: location}});
+          }
+        });
+      }
+    }
   return (
     <div className="card bg-base-200 relative">
       <figure>
@@ -10,7 +58,7 @@ const FoodCard = ({item}) => {
         <h2 className="text-3xl font-bold">{name}</h2>
         <p>{recipe}</p>
         <div className="card-actions justify-center">
-          <button className="btn btn-outline bg-slate-200 border-0 hover:text-amber-400 border-b-4 text-amber-400 border-b-amber-400 uppercase">
+          <button onClick={()=> handleAddToCart(item)} className="btn btn-outline bg-slate-200 border-0 hover:text-amber-400 border-b-4 text-amber-400 border-b-amber-400 uppercase">
             Add to cart
           </button>
         </div>
--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -1,59 +1,117 @@
 import { NavLink } from "react-router-dom";
 import useAuth from "../../../hooks/useAuth";
+import { FaShoppingCart } from "react-icons/fa";
 
 const Navbar = () => {
-  const {user, logOut} = useAuth();
+  const { user, logOut } = useAuth();
 
-  const handleLogOut = ()=>{
+  const handleLogOut = () => {
     logOut()
-    .then(res => console.log(res))
-    .catch(err => console.log(err))
-  }
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  };
 
   const navItems = (
     <>
       <li>
-        <NavLink to='/'
-        className={({ isActive, isPending }) =>
-            isPending ? "pending" : isActive ? "text-yellow-500 font-bold" : "text-white"
+        <NavLink
+          to="/"
+          className={({ isActive, isPending }) =>
+            isPending
+              ? "pending"
+              : isActive
+              ? "text-yellow-500 font-bold"
+              : "text-white"
           }
-        >Home</NavLink>
+        >
+          Home
+        </NavLink>
       </li>
       <li>
-        <NavLink to='/contact'
-        className={({ isActive, isPending }) =>
-            isPending ? "pending" : isActive ? "text-yellow-500 font-bold" : "text-white"
+        <NavLink
+          to="/contact"
+          className={({ isActive, isPending }) =>
+            isPending
+              ? "pending"
+              : isActive
+              ? "text-yellow-500 font-bold"
+              : "text-white"
           }
-        >Contact Us</NavLink>
+        >
+          Contact Us
+        </NavLink>
       </li>
       <li>
-        <NavLink to='/dashboard'
-        className={({ isActive, isPending }) =>
-            isPending ? "pending" : isActive ? "text-yellow-500 font-bold" : "text-white"
+        <NavLink
+          to="/dashboard"
+          className={({ isActive, isPending }) =>
+            isPending
+              ? "pending"
+              : isActive
+              ? "text-yellow-500 font-bold"
+              : "text-white"
           }
-        >Dashboard</NavLink>
+        >
+          Dashboard
+        </NavLink>
       </li>
       <li>
-        <NavLink to='/menu'
-        className={({ isActive, isPending }) =>
-            isPending ? "pending" : isActive ? "text-yellow-500 font-bold" : "text-white"
+        <NavLink
+          to="/menu"
+          className={({ isActive, isPending }) =>
+            isPending
+              ? "pending"
+              : isActive
+              ? "text-yellow-500 font-bold"
+              : "text-white"
           }
-        >Our Menu</NavLink>
+        >
+          Our Menu
+        </NavLink>
       </li>
       <li>
-        <NavLink to='/order/salad'
-        className={({ isActive, isPending }) =>
-            isPending ? "pending" : isActive ? "text-yellow-500 font-bold" : "text-white"
+        <NavLink
+          to="/order/salad"
+          className={({ isActive, isPending }) =>
+            isPending
+              ? "pending"
+              : isActive
+              ? "text-yellow-500 font-bold"
+              : "text-white"
           }
-        >Order Food</NavLink>
+        >
+          Order Food
+        </NavLink>
       </li>
       <li>
-       {
-        user ?  <button onClick={handleLogOut} className="btn" to='/login'>Log Out</button> :  <NavLink className="btn" to='/login'>Login</NavLink>
-       }
+        <NavLink
+          to="/order/salad"
+          className={({ isActive, isPending }) =>
+            isPending
+              ? "pending"
+              : isActive
+              ? "text-yellow-500 font-bold"
+              : "text-white"
+          }
+        >
+          <button className="flex">
+            <FaShoppingCart></FaShoppingCart>
+            <div className="badge ml-1 badge-secondary">+0</div>
+          </button>
+        </NavLink>
       </li>
 
-      
+      <li>
+        {user ? (
+          <button onClick={handleLogOut} className="btn" to="/login">
+            Log Out
+          </button>
+        ) : (
+          <NavLink className="btn" to="/login">
+            Login
+          </NavLink>
+        )}
+      </li>
     </>
   );
 
@@ -85,17 +143,14 @@ const Navbar = () => {
               {navItems}
             </ul>
           </div>
-          <a className="text-left font-semibold text-xl">Bistro Boss
-            <br /> 
-            R e s t a u r a n t
+          <a className="text-left font-semibold text-xl">
+            Bistro Boss
+            <br />R e s t a u r a n t
           </a>
         </div>
-        
+
         <div className="navbar-end hidden lg:flex">
-        <ul className="flex items-center gap-4 px-1">
-          {navItems}
-          
-          </ul>
+          <ul className="flex items-center gap-4 px-1">{navItems}</ul>
         </div>
       </div>
     </>
