d39d32a5d8ddcd85eb3724209307a492173b0d38
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-10-30 19:59:10 +0600

    google signin is successful

--- src/components/SocialLogin/SocialLogin.jsx
+++ src/components/SocialLogin/SocialLogin.jsx
@@ -1,20 +1,51 @@
 import { FaGoogle } from "react-icons/fa";
 import useAuth from "../../hooks/useAuth";
+import useAxiosPublic from "../../hooks/useAxiosPublic";
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
 
 const SocialLogin = () => {
     const {signInWithGoogle} = useAuth();
+    const axiosPublic = useAxiosPublic();
+    const navigate = useNavigate();
 
     const handleGoogleSignIn = ()=>{
         signInWithGoogle()
         .then(result=>{
             const user = result.user;
             console.log(user);
+            const userInfo = {
+                email: result.user?.email,
+                name: result.user?.displayName
+            }
+            axiosPublic.post('/users', userInfo)
+            .then(res =>{
+                if(res.data.insertedId){
+                    Swal.fire({
+                        position: "center",
+                        icon: "success",
+                        title: "User has been saved",
+                        showConfirmButton: false,
+                        timer: 1500
+                      });
+                      navigate('/');
+                } else {
+                    Swal.fire({
+                        position: "center",
+                        icon: "success",
+                        title: "User already exists",
+                        showConfirmButton: false,
+                        timer: 1500
+                      });
+                      navigate('/');
+                }
+            })
         })
     }
   return (
     <div>
       <div>
-        <button className="btn">
+        <button onClick={handleGoogleSignIn} className="btn">
           <FaGoogle></FaGoogle>
           Login with Google
         </button>
--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,7 +1,6 @@
 import { createContext, useState, useEffect } from "react";
-import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, GoogleAuthProvider } from "firebase/auth";
 import { app } from './../firebase/firebase.config';
-import { GoogleAuthProvider } from "firebase/auth/web-extension";
 
 export const AuthContext = createContext(null);
 
