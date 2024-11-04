3b01d35b19aa8e11b35d6877f5f37e538639cde7
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-11-04 13:57:02 +0600

    added bar chart

--- src/pages/Dashboard/AdminHome/AdminHome.jsx
+++ src/pages/Dashboard/AdminHome/AdminHome.jsx
@@ -3,6 +3,10 @@ import useAuth from "../../../hooks/useAuth";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import { FaBook, FaCarSide, FaDollarSign, FaUsers } from "react-icons/fa";
 import { IoFastFood } from "react-icons/io5";
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid } from 'recharts';
+
+const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red'];
+
 
 const AdminHome = () => {
   const { user } = useAuth();
@@ -23,6 +27,19 @@ const AdminHome = () => {
       return res.data;
     },
   });
+
+  const getPath = (x, y, width, height) => {
+    return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3}
+    ${x + width / 2}, ${y}
+    C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${x + width}, ${y + height}
+    Z`;
+  };
+  
+  const TriangleBar = (props) => {
+    const { fill, x, y, width, height } = props;
+  
+    return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+  };
 
   
   return (
@@ -65,6 +82,32 @@ const AdminHome = () => {
           <div className="stat-value">{stats?.orders}</div>
         </div>
       </div>
+
+     <div className='flex'>
+        <div className='w-1/2'>
+        <BarChart
+      width={500}
+      height={300}
+      data={chartData}
+      margin={{
+        top: 20,
+        right: 30,
+        left: 20,
+        bottom: 5,
+      }}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="category" />
+      <YAxis />
+      <Bar dataKey="quantity" fill="#8884d8" shape={<TriangleBar />} label={{ position: 'top' }}>
+        {chartData.map((entry, index) => (
+          <Cell key={`cell-${index}`} fill={colors[index % 6]} />
+        ))}
+      </Bar>
+    </BarChart>
+        </div>
+        <div className='w-1/2'></div>
+     </div>
     </div>
   );
 };
